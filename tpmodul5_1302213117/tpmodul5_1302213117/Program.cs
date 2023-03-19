3407023cf44f05c8e0635d91ccd70f870996d80b
Regyyyy <regyrahman@gmail.com>
2023-03-19 17:46:13 +0700

    Menambahkan class HaloGeneric
    menambahkan class HaloGeneric dengan method
    SapaUser

--- tpmodul5_1302213117/tpmodul5_1302213117/Program.cs
+++ tpmodul5_1302213117/tpmodul5_1302213117/Program.cs
@@ -1,7 +1,22 @@
-﻿internal class Program
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        HaloGeneric<string> gen = new HaloGeneric<string>();
+        gen.SapaUser("Regy");
+    }
+}
+
+public class HaloGeneric<T>
+{
+    /*
+    public keyValue[] Data;
+    public int index;
+    */
+    public void SapaUser(T user)
+    {
+        Console.WriteLine("Halo user " + user);
     }
 }
