ac1a2ef0e5d069f4182c372dfd48bfeac07fa6df
Regyyyy <regyrahman@gmail.com>
2023-03-19 18:27:21 +0700

    “menambahkan class DataGeneric
    commit kedua dengan membuat class DataGeneric

--- tpmodul5_1302213117/tpmodul5_1302213117/Program.cs
+++ tpmodul5_1302213117/tpmodul5_1302213117/Program.cs
@@ -2,6 +2,22 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        DataGeneric<string> siswa = new DataGeneric<string>("1302213117");
+        siswa.PrintData();
+    }
+}
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: "+data);
     }
 }
